225d4e79803c7155c15f42e0230f442a82d939ba
tuananhnt <tuananh@unstatic.co>
2025-02-14 05:58:37 +0700

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,22 @@ import Home from "./home";
 export const metadata: Metadata = {
   title: "Happy Valentine's Day <3 💕",
   description: "Valentine's Day",
+  openGraph: {
+    title: "Happy Valentine's Day <3 💕",
+    description: "Valentine's Day",
+    url: "/letter2-removebg-preview-crop",
+    siteName: "https://my-love1402.vercel.app/",
+    images: [
+      {
+        url: "/letter2-removebg-preview-crop", // Ensure this is a valid URL
+        width: 1200,
+        height: 630,
+        alt: "Valentine's Day",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function HomePage() {
